754ea7b89138336a8a86d9b48a75eaa99e961393
Philippe Charles <philippe.charles@nbb.be>
2025-03-18 10:52:29 +0100

    Replace json with csv

--- src/main/java/MakeTable.java
+++ src/main/java/MakeTable.java
@@ -1,14 +1,16 @@
-import com.google.gson.Gson;
-
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
@@ -23,7 +25,7 @@ public class MakeTable {
         System.out.println("| | " + String.join(" | ", versions.keySet()) + " |");
         System.out.println("| " + IntStream.range(0, versions.size() + 1).mapToObj(i -> "---").collect(Collectors.joining(" | ")) + " |");
         plugins.forEach((plugin, items) -> {
-            System.out.print("| " + plugin + " " + items.stream().map(item -> item.plugin_version()).findFirst().orElse(""));
+            System.out.print("| " + plugin + " " + items.stream().map(Item::plugin_version).findFirst().orElse(""));
             versions.keySet().stream()
                     .map(version -> items.stream().filter(z -> z.version().equals(version)).findFirst().map(Item::exitcode).orElse(-1))
                     .map(MakeTable::emoji)
@@ -42,16 +44,21 @@ public class MakeTable {
 
     private static List<Item> parse(Path file) throws IOException {
         try (var reader = Files.newBufferedReader(file)) {
-            return Arrays.asList(new Gson().fromJson(reader, Item[].class));
+            return reader.lines().map(Item::parse).toList();
         }
     }
 
     private static List<Item> extract() throws IOException {
-        try (var reader = new InputStreamReader(Objects.requireNonNull(MakeTable.class.getResourceAsStream("test.json")), StandardCharsets.UTF_8)) {
-            return Arrays.asList(new Gson().fromJson(reader, Item[].class));
+        try (var reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(MakeTable.class.getResourceAsStream("test.csv")), UTF_8))) {
+            return reader.lines().map(Item::parse).toList();
         }
     }
 
     public record Item(int exitcode, String plugin, String version, String plugin_version) {
+
+        static Item parse(String line) {
+            String[] array = line.split(",", -1);
+            return new Item(Integer.parseInt(array[0]), array[1], array[2], array[3]);
+        }
     }
 }
